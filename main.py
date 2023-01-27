66c3ab04885171cb4e704e548976f7196d249f4e
Marc Orós <marcoc619@gmail.com>
2023-01-27 11:22:54 +0100

    feature: Improved serial connection error handling and removed old logic to calculate RPM LEDs

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ def check_iracing():
         state.ir_connected = False
         # don't forget to reset all your in State variables
         state.last_car_setup_tick = -1
-        global maxrpm
-        maxrpm = 1
         # we shut down ir library (clear all internal variables)
         ir.shutdown()
         print('irsdk disconnected')
@@ -25,41 +23,39 @@ def check_iracing():
 
 
 def loop():
-    global maxrpm
     speed = "{:03d}".format(int(ir['Speed']*3.6))
     rpm = "{:05d}".format(int(ir['RPM']))
     gear = ir['Gear'] + 1
-    maxrpm = max(maxrpm, ir['RPM'])
-    rpm_thr = maxrpm*0.75
-    curr_thr = int(rpm) - rpm_thr
-    if int(rpm) < rpm_thr:
-        led = 0
-    else:
-        led = int(min((curr_thr+50)/(maxrpm-rpm_thr), 1)*8)
+    led = int(8*ir['ShiftIndicatorPct'])
     buf = str(led) + ' ' + str(speed) + ' ' + str(rpm) + ' ' + str(gear)
     final = buf.encode('ascii')
     cnt=ser.write(final)
 
 
-ser = serial.Serial('COM3', 9600)
 ir = irsdk.IRSDK()
 state = State()
 
 maxrpm = 1
 led = 0
 
-try:
-    # infinite loop
-    while True:
-        # check if we are connected to iracing
-        check_iracing()
-        # if we are, then process data
-        if state.ir_connected:
-            loop()
-        # sleep for 1 second
-        # maximum you can use is 1/60
-        # cause iracing update data with 60 fps
-        time.sleep(1/60)
-except KeyboardInterrupt:
-    # press ctrl+c to exit
-    pass
+while True:
+    try:
+        ser = serial.Serial('COM3', 9600)
+        print("Serial connection established")
+        # infinite loop
+        while True:
+            # check if we are connected to iracing
+            check_iracing()
+            # if we are, then process data
+            if state.ir_connected:
+                loop()
+            # sleep for 1 second
+            # maximum you can use is 1/60
+            # cause iracing update data with 60 fps
+            time.sleep(1/60)
+    except KeyboardInterrupt:
+        # press ctrl+c to exit
+        break
+    except serial.serialutil.SerialException:
+        time.sleep(1)
+
